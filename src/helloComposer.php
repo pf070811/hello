a7f0d7a0c9da626ce5d549f5b1c1b87163eeaf62
pf070811 <wph45@163.com>
2018-08-01 13:58:23 +0800

    first commit

---
+++ src/helloComposer.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: wangpenghai
+ * Date: 2018/8/1
+ * Time: 上午10:08
+ */
+namespace Hello;
+
+class helloComposer
+{
+    public function sayHello()
+    {
+        return 'hello composer';
+    }
+}
